dd8ae38e05ab21a62eef86ba068e905c35ae6765
buyaobishige <1105936025@163.com>
2019-11-10 10:49:03 +0800

    a

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,6 +1,33 @@
+let dices = [false, false, false, false, false, false];
+let recentValue = null;
+let value = null;
 Page({
   data: {
-    dices: [1,2,3,4,5,6]
+    dices: [false, false, false, false, false, false],
+    showTip: true,
+  },
+  randomValue() {
+    let rand = Math.floor(1 + Math.random() * (5));
+    while (rand == recentValue) {
+      rand = Math.floor(1 + Math.random() * (5));
+    };
+    value = rand
+  },
+  startDice() {
+    new Promise((resolve, reject) => {
+      for (let i = 0; i < 6; i++) {
+        dices[i] = false
+      }
+      return resolve(1)
+    }).then(() => {
+      this.randomValue();
+      console.log(value);
+      dices[value] = true;
+      recentValue = value;
+      this.setData({
+        dices,
+        showTip: false
+      })
+    })
   }
-
 })
