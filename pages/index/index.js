a22a21896fffbf85629842fe0e451e87f4e00391
buyaobishige <1105936025@163.com>
2019-11-12 10:15:29 +0800

    .

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,39 @@
-let dices = [false, false, false, false, false, false];
+let dices = [{
+    show: false,
+    mode: false
+  },
+  {
+    show: false,
+    mode: false
+  },
+  {
+    show: false,
+    mode: false
+  },
+  {
+    show: false,
+    mode: false
+  },
+  {
+    show: false,
+    mode: false
+  },
+  {
+    show: false,
+    mode: false
+  },
+];
 let recentValue = null;
 let value = null;
+let x = 3;
 Page({
   data: {
-    dices: [false, false, false, false, false, false],
+    x,
+    tip: "需要我来帮你？\n",
+    dices,
     showTip: true,
+    showClick: true,
+    value: 0
   },
   randomValue() {
     let rand = Math.floor(1 + Math.random() * (5));
@@ -14,20 +43,46 @@ Page({
     value = rand
   },
   startDice() {
-    new Promise((resolve, reject) => {
+    if (x < 1) {
       for (let i = 0; i < 6; i++) {
-        dices[i] = false
-      }
-      return resolve(1)
-    }).then(() => {
-      this.randomValue();
-      console.log(value);
-      dices[value] = true;
-      recentValue = value;
+        dices[i].show = false;
+      };
       this.setData({
+        x: 0,
         dices,
-        showTip: false
+        tip: "选恐患者下顿饭再来吧",
+        showTip: true,
+        showClick: false
+      });
+    } else {
+      if (x == 3) {
+        wx.showLoading({
+          title: "加载中...",
+          mask: true
+        })
+        setTimeout(() => {
+          wx.hideLoading()
+        }, 1000)
+      }
+
+      x--;
+      new Promise((resolve, reject) => {
+        for (let i = 0; i < 6; i++) {
+          dices[i].show = false;
+        }
+        return resolve(1)
+      }).then(() => {
+        this.randomValue();
+        dices[value].show = true;
+        dices[value].mode = !dices[value].mode;
+        recentValue = value;
+        this.setData({
+          dices,
+          showTip: false,
+          x,
+          value: value + 1
+        })
       })
-    })
+    }
   }
 })
