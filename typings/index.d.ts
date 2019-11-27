eb3032a9aa694a0c89c88609c1cf9cd66511888d
Mr.Hope <zhangbowang1998@gmail.com>
2019-11-27 22:34:56 +0800

    reorginize folder

---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable multiline-comment-style */
+/* eslint-disable spaced-comment */
+/* eslint-disable @typescript-eslint/triple-slash-reference */
+/// <reference path="../node_modules/miniprogram-api-typings/index.d.ts" />
+/// <reference path="./wx/lib.wx.event.d.ts" />
